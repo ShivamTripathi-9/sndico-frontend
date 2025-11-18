ed5682bcdd35f09fb6d61c5acc88261ca8842f3f
Shivam Tripathi <shivamtripathi93758@gmail.com>
2025-11-18 21:05:35 +0530

    Navbar add

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'   ;
-import NavigationBar from '../components/NavigationBar'   ;
-import SubcategoryView from '../components/SubcategoryView';  
+
 
 export default function HomePage() {
   return (
     <>
       <Navbar />  
-      {/* <NavigationBar />
-      <SubcategoryView/> */}
+    
 
 
     </>
